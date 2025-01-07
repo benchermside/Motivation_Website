77b32065b15c2cdfd0adbcdf92dbd018134fb639
benchermside <bchermside@gmail.com>
2025-01-07 15:32:29 -0500

    trying

--- tasks.js
+++ tasks.js
@@ -1,8 +1,17 @@
 const tasks = [];
 
+
 function openTasks(){
     /**
      * runs when tasks bar opened
     */
-    console.log("opened");
+    console.log("opened");//delete em
+    const taskDisplays = document.createElement("div");
+    taskDisplays.classList.add("taskSection")
+    const body = document.getElementById("body");
+    body.appendChild(taskDisplays);
+
 }
+
+
+
