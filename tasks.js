70e5a81fec55c9977e09f1c6efbbceca57208dce
benchermside <bchermside@gmail.com>
2025-01-07 15:41:18 -0500

    stuff

--- tasks.js
+++ tasks.js
@@ -7,7 +7,13 @@ function openTasks(){
     */
     console.log("opened");//delete em
     const taskDisplays = document.createElement("div");
-    taskDisplays.classList.add("taskSection")
+    taskDisplays.classList.add("taskSection");
+    if (tasks.length === 0){//message for when
+        const noTaskMessage = document.createElement("div");
+        noTaskMessage.innerText = "You have no tasks, go add some.";
+        taskDisplays.appendChild(noTaskMessage);
+    }
+
     const body = document.getElementById("body");
     body.appendChild(taskDisplays);
 
