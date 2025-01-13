1f11cf4cd3b66e1420478312db931b9319274b02
Nat <nhoward@oberlin.edu>
2025-01-13 15:34:12 -0500

    wheel with lines

--- rewards.js
+++ rewards.js
@@ -5,9 +5,19 @@ function openRewards(){
     const wheelToSpin = document.createElement("div");
     wheelToSpin.classList.add("wheelToSpin");
     body.appendChild(wheelToSpin);
-    const line1 = document.createElement("div");
-    line1.classList.add("line1");
-    wheelToSpin.appendChild(line1);
+    // const line1 = document.createElement("div");
+    // line1.classList.add("line");
+    // const line2 = document.createElement("div");
+    // line2.classList.add("line");
+    const numLines = 12
+    for (i=0; i<numLines; i++){
+        const thisLine = document.createElement("div");
+        thisLine.classList.add("line");
+        wheelToSpin.appendChild(thisLine);
+        thisLine.style.rotate = `${(360/numLines)*i}deg`;
+    }
+    // wheelToSpin.appendChild(line1);
+    // wheelToSpin.appendChild(line2);
     wheelToSpin.id = "wheelToSpin"
     let element = document.getElementById("wheelToSpin");
 
