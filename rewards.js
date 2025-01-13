764a18c5b3a95ddeea56400d49f3d9eed98754de
Nat <nhoward@oberlin.edu>
2025-01-13 13:36:07 -0500

    circle with line in wrong place/on top

--- rewards.js
+++ rewards.js
@@ -1,5 +1,12 @@
 function openRewards(){
     /* opens reward page*/
     deleatOpenPage();
-    console.log("called")
+    console.log("called");
+    const wheelToSpin = document.createElement("div");
+    wheelToSpin.classList.add("wheelToSpin");
+    body.appendChild(wheelToSpin);
+    const line1 = document.createElement("div");
+    line1.classList.add("line1");
+    wheelToSpin.appendChild(line1);
+
 }
