ee675ca5b7ad11243dd914a52980981ca766821d
benchermside <bchermside@gmail.com>
2025-01-13 17:06:44 -0500

    test

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 //page for handling sign in with google
 function sighInLoad(){//runs on page load
     console.log("load sign in");
-    // gapi.load("auth2", function(){//note that I dont know what I am doing
-    //     gapi.auth2.init();
-    // });
+    gapi.load("auth2", function(){//note that I dont know what I am doing
+        gapi.auth2.init();
+    });
 }
 
 function onSignIn(googleUser){
