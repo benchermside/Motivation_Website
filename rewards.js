042ba9a638f45c26bf5cd811046e3b03d1aff4d2
Nat <nhoward@oberlin.edu>
2025-01-14 18:05:56 -0500

    not working display won

--- rewards.js
+++ rewards.js
@@ -8,6 +8,7 @@ function openRewards(){
     wheelToSpin.classList.add("wheelToSpin");
     body.appendChild(wheelToSpin);
     const numLines = 12
+    let degList = []
     for (i=0; i<numLines; i++){
         const thisLine = document.createElement("div");
         thisLine.classList.add("line");
@@ -27,8 +28,13 @@ function openRewards(){
         wheelToSpin.appendChild(thisImage);
         thisImage.style.left = `${halfWeelLength - halfImageHeight + (imageFromCenterLength*Math.cos(((Math.PI*2)/(numLines*2)) +(((i*360)/numLines)*(Math.PI/180))))}px`;
         thisImage.style.top = `${halfWeelLength - halfImageHeight + (imageFromCenterLength*Math.sin((((Math.PI*2)/(numLines*2)))+(((i*360)/numLines)*(Math.PI/180))))}px`;        
+        degList.push({degrees: ((360/numLines)*i), image: thisImage.src})
     }
-    
+    console.log(degList);
+    const arrow = document.createElement("div");
+    arrow.classList.add("arrow")
+    body.appendChild(arrow);
+
     wheelToSpin.id = "wheelToSpin";
     window.onresize = function(){ 
         openRewards();
@@ -40,16 +46,34 @@ function openRewards(){
         wheelToSpin.classList.remove("runAnimation");
         void wheelToSpin.offsetWidth;
         wheelToSpin.classList.add("runAnimation");
-        const randomDegree = (Math.floor(Math.random()*180))*2 + 361 
-        wheelToSpin.style.setProperty("--rotation-deg", randomDegree + "deg");
-            /*citation: 
+        /*citation for lines 45-48: 
             Title: answer to StackOverflow question: CSS Animation onClick
             Author: sad comrade
             Date: October 12, 2019
             Code Version: 1.0
             Link: https://stackoverflow.com/a/58353279 */
-    
+        const randomDegree = (Math.floor(Math.random()*180))*2 + 361 
+        console.log(randomDegree)
+        wheelToSpin.style.setProperty("--rotation-deg", randomDegree + "deg");
+        const thisDegree = (randomDegree - 360);
+        console.log(thisDegree)
+        for (let i=0; i<(numLines-1); i++){
+            if(thisDegree<degList[i+1].degrees&&thisDegree>=degList[i].degrees){
+                let wonImage = document.createElement("img");
+                wonImage.src = degList[i].image;
+                let iI = i+4
+                if (iI >= 12){
+                    iI = iI - 12
+                }
+                console.log(degList[iI])
+                wonImage.id = "wonImage"
+                body.appendChild(wonImage);
+                wonImage.classList.add("winner");
+            }
+        } 
     })  
+    
+    
 
 }
 
