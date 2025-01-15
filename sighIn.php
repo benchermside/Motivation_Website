e03b1fceb2985a53bf03974ff6bec4535cf4972f
benchermside <bchermside@gmail.com>
2025-01-15 14:25:56 -0500

    working

--- sighIn.php
+++ sighIn.php
@@ -1,2 +1,20 @@
 <?php
 echo "hi world";
+include("./pass.php");
+$servername = "localhost";
+$username = "bchermsi";
+$password = $pass;
+$dbname = "bchermsi";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+// $sql = "";
+
+$result = $conn->query($sql);
+$conn->close();
