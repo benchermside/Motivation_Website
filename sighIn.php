eb1f55803862f978688f754b8ae78232ed2a628b
benchermside <bchermside@gmail.com>
2025-01-15 15:09:45 -0500

    working on sign in

--- sighIn.php
+++ sighIn.php
@@ -6,14 +6,15 @@ $SQLusername = "bchermsi";
 $SQLpassword = getpass();
 $SQLdbname = "bchermsi";
 
-$conn = new mysqli($SQLservername, $SQLusername, $SQLpassword, $SQLdbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else{
-    echo"connected";
-}
+//$conn = new mysqli($SQLservername, $SQLusername, $SQLpassword, $SQLdbname);
+$conn = new PDO("mysql:host=$SQLservername;dbname=$SQLdbname",
+$user = $SQLusername, $pw = $SQLpassword);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// else{
+//     echo"connected";
+// }
 
 $username = $_GET["userName"];
 $password = $_GET["password"];
@@ -26,4 +27,4 @@ echo $newUser;
 // $sql = "";
 
 // $result = $conn->query($sql);
-$conn->close();
+// $conn->close();
