7f5f25c33b450c4fc261654d27cd2405ebef0b38
benchermside <bchermside@gmail.com>
2025-01-23 17:13:40 -0500

    test

--- rewards.php
+++ rewards.php
@@ -6,6 +6,36 @@ error_reporting(-1);
 
 $rewardImage = $_POST["rewardImage"];
 
+require "pass.php";
+$SQLservername = "sql.cs.oberlin.edu";
+$SQLusername = "bcherm";
+$SQLpassword = getpass();
+$SQLdbname = "motivationDatabase";
+
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:motivation-database-server.database.windows.net,1433; Database = motivationDatabase", "bcherm", $SQLpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print "Error connecting to SQL Server.";
+    die(print_r($e));
+}
+
+
+
+$stmt = $conn->prepare("SELECT token from users WHERE userName=:username;");
+$stmt->bindparam("username", $username, PDO::PARAM_STR);
+$stmt->execute();
+$savedTokenList = $stmt->fetchAll();
+$savedToken = $savedTokenList[0]["token"];
+if($savedToken === $gotToken){
+
+}
+else{
+    print "authentication error";
+}
+
 
 
 print $rewardImage;
