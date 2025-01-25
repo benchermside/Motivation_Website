3c5413ed96b2e0ab0c4522118869415d2f941b9b
benchermside <bchermside@gmail.com>
2025-01-25 12:08:28 -0500

    bigFix?

--- rewards.php
+++ rewards.php
@@ -16,6 +16,7 @@ $username = $_POST["userName"];
 $gotToken = $_POST["token"];
 $rewardSRC = $_POST["rewardImage"];
 $newNumSpins = $_POST["numSpins"];
+$gotTryAgain = $_POST["gotTryAgain"];
 
 
 
@@ -29,6 +30,19 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
+function decrementNumSpins($usernameToDecrement, $conn) {
+    $getNumSpins = $conn->prepare(query: "SELECT numSpins FROM users WHERE userName=:userName;");
+    $getNumSpins->bindparam("userName", $usernameToDecrement);
+    $getNumSpins->execute();
+    $numSpinsQuerryResult = $getNumSpins->fetchAll();
+    $numSpinsResult = intval($numSpinsQuerryResult[0]["numSpins"]);
+    $decrementedNumSpins = $numSpinsResult - 1;
+    $updateNumSpins = $conn->prepare("INSERT INTO users (numSpins) VALUES (:newNumSpins) WHERE userName=:userName;");
+    $updateNumSpins->bindparam("newNumSpins", $decrementedNumSpins, PDO::PARAM_STR);
+    $updateNumSpins->bindparam("userName", $usernameToDecrement, PDO::PARAM_STR);
+    $updateNumSpins->execute();
+}
+
 
 
 $stmt = $conn->prepare("SELECT token from users WHERE userName=:username;");
@@ -37,41 +51,55 @@ $stmt->execute();
 $savedTokenList = $stmt->fetchAll();
 $maxNumTrys = 30;//after this menay trys at genorating a unuque token the program exits having failed
 $savedToken = $savedTokenList[0]["token"];
-if($savedToken === $gotToken){
-    $numTrys = 0;
-    $stillSerching = true;
-    $randomID = null;
-    while($numTrys<$maxNumTrys && $stillSerching){
-        $randomID = random_int(-2147483647, 2147483646);
-        $IDCheck = $conn->prepare("SELECT UserRewardID FROM rewards WHERE UserRewardID=:rewardID;");
-        $IDCheck->bindparam("rewardID", $randomID, PDO::PARAM_INT);
-        $IDCheck->execute();
-        $allResults = $IDCheck->fetchAll();
-        if(count($allResults) === 0){
-            $stillSerching = false;
+
+if($gotTryAgain === "no"){
+    if($savedToken === $gotToken){
+        $numTrys = 0;
+        $stillSerching = true;
+        $randomID = null;
+        while($numTrys<$maxNumTrys && $stillSerching){
+            $randomID = random_int(-2147483647, 2147483646);
+            $IDCheck = $conn->prepare("SELECT UserRewardID FROM rewards WHERE UserRewardID=:rewardID;");
+            $IDCheck->bindparam("rewardID", $randomID, PDO::PARAM_INT);
+            $IDCheck->execute();
+            $allResults = $IDCheck->fetchAll();
+            if(count($allResults) === 0){
+                $stillSerching = false;
+            }
+            $numTrys++;
         }
-        $numTrys++;
-    }
-    if($numTrys === $maxNumTrys && $stillSerching === true){
-        print "server error try again";
+        if($numTrys === $maxNumTrys && $stillSerching === true){
+            print "server error try again";
+        }
+        else{
+            $stmt = $conn->prepare("INSERT INTO rewards (UserRewardID, userName, src) VALUES (:rewardID, :userName, :src);");
+            $stmt->bindparam("rewardID", $randomID, PDO::PARAM_STR);
+            $stmt->bindparam("userName", $username, PDO::PARAM_STR);
+            $stmt->bindparam("src", $rewardSRC, PDO::PARAM_STR);
+            $stmt->execute();
+            decrementNumSpins($username, $conn);
+
+            // $getNumSpins = $conn->prepare(query: "SELECT numSpins FROM users WHERE userName=:userName;");
+            // $getNumSpins->bindparam("userName", $username);
+            // $getNumSpins->execute();
+            // $numSpinsQuerryResult = $getNumSpins->fetchAll();
+            // $numSpinsResult = intval($numSpinsQuerryResult[0]["numSpins"]);
+            // $decrementedNumSpins = $numSpinsResult - 1;
+            // $updateNumSpins = $conn->prepare("INSERT INTO users (numSpins) VALUES (:newNumSpins) WHERE userName=:userName;");
+            // $updateNumSpins->bindparam("newNumSpins", $decrementedNumSpins, PDO::PARAM_STR);
+            // $updateNumSpins->bindparam("userName", $username, PDO::PARAM_STR);
+            // $updateNumSpins->execute();
+
+            //print $newNumSpins;
+        }
+
     }
     else{
-        $stmt = $conn->prepare("INSERT INTO rewards (UserRewardID, userName, src) VALUES (:rewardID, :userName, :src);");
-        $stmt->bindparam("rewardID", $randomID, PDO::PARAM_STR);
-        $stmt->bindparam("userName", $username, PDO::PARAM_STR);
-        $stmt->bindparam("src", $rewardSRC, PDO::PARAM_STR);
-        $stmt->execute();
-        $updateNumSpins = $conn->prepare("INSERT INTO users (numSpins) VALUES (:newNumSpins) WHERE userName=:userName;");
-        $updateNumSpins->bindparam("newNumSpins", $newNumSpins, PDO::PARAM_STR);
-        $updateNumSpins->bindparam("userName", $username, PDO::PARAM_STR);
-        $updateNumSpins->execute();
-
+        print "authentication error";
     }
-
 }
 else{
-    print "authentication error";
+    decrementNumSpins($username, $conn);
 }
-
 
 
